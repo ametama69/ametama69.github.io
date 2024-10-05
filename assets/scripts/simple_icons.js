6edeadd01ec6e286f829be9afeb50d01eea9ceb8
ametama69 <92367442+ametama69@users.noreply.github.com>
2024-10-06 01:48:29 +0900

    Initial commit

---
+++ assets/scripts/simple_icons.js
@@ -0,0 +1,34 @@
+const style = document.createElement('style');
+style.textContent = `
+.si::before{ 
+  content: '';
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+`;
+
+
+for (const iconname of ["bluesky","niconico","github","instagram","misskey","mastodon","activitypub","scrapbox","x","rss"]) {
+  style.textContent += `
+.si-${iconname}::before{
+  background-image: url(https://cdn.simpleicons.org/${iconname});
+}
+`;
+}
+
+style.textContent += `
+.si-wh::before{
+  filter: brightness(90%) invert(1) brightness(1000%);
+}
+
+.si-bk::before{
+  filter: brightness(0);
+}
+`;
+
+document.head.appendChild(style);
+
